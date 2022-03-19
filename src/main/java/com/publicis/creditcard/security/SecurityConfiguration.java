4e8bf96900e981f9b3c8a8172a3f8d7ce4d92f4a
AnvyKrish <anvithakrishna10@gmail.com>
2022-03-19 19:18:23 +0000

    Implemented swagger

--- src/main/java/com/publicis/creditcard/security/SecurityConfiguration.java
+++ src/main/java/com/publicis/creditcard/security/SecurityConfiguration.java
@@ -20,14 +20,22 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 				.antMatchers("/api/v1/addCard").permitAll()
 				.antMatchers("/api/v1/getAllCards").permitAll()
 				.antMatchers("/h2-console/**/**").permitAll()
+				.antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources", "/configuration/security", "/swagger-ui.html", "/webjars/**").permitAll()
 				.anyRequest().authenticated().and().exceptionHandling()
 				.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
+	private static final String[] AUTH_WHITELIST = {
+	        "/swagger-resources/**",
+	        "/swagger-ui.html",
+	        "/v2/api-docs",
+	        "/webjars/**"
+	};
+	
 	@Override
 		public void configure(WebSecurity web) throws Exception {
 			web.ignoring().antMatchers("/h2-console/**");
-
+			 web.ignoring().antMatchers(AUTH_WHITELIST);
 	}
 
 }
